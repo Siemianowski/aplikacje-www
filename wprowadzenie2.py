bbd24884cfdf11173eefbff2eab974b2c749927d
zakowski <zakosh0@gmail.com>
2019-10-24 12:52:19 +0200

    Update wprowadzenie2.py

--- wprowadzenie2.py
+++ wprowadzenie2.py
@@ -1,1 +1,16 @@
-123
+def function_1(lista_1, lista_2):
+    lista_3 = []
+    for i in range(0, len(lista_1)):
+        if i % 2:
+            lista_3.append(lista_1[i])
+    for i in range(0, len(lista_2)):
+        if not i % 2:
+            lista_3.append(lista_2[i])
+    return lista_3
+
+
+lista_1 = [i for i in range(1, 16)]
+lista_2 = [i for i in range(23, 39)]
+print(function_1(lista_1, lista_2))
+
+
